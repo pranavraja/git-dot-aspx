b6b5ea99f058a621a51d89bcba16b476c5e35dac
Pranav Raja <rickdangerous1@gmail.com>
2012-07-31 16:56:06 +1000

    minor change to the UI

--- GitAspx/AssemblyInfo.cs
+++ GitAspx/AssemblyInfo.cs
@@ -4,9 +4,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GitAspx")]
 [assembly: AssemblyDescription("ASP.NET Implementation of git-http-backend")]
-[assembly: AssemblyCompany("Jeremy Skinner")]
+[assembly: AssemblyCompany("PMoney")]
 [assembly: AssemblyProduct("GitAspx")]
-[assembly: AssemblyCopyright("Copyright © Jeremy Skinner 2010")]
+[assembly: AssemblyCopyright("Copyright © PMoney 2012")]
 [assembly: ComVisible(false)]
 [assembly: Guid("087abc38-5949-4dbe-9549-9be40bc57cc3")]
 [assembly: AssemblyVersion("0.3.0.0")]
